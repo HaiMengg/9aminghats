45fa10fb7f02434679ff451de86870cfc58c1579
HaiMengg <mikegaming400@gmail.com>
2021-11-13 10:48:53 +0700

    Update HP

--- js/js_function_AK.js
+++ js/js_function_AK.js
@@ -1,3 +1,75 @@
-function AddComponents() {
-    
+function CreateCarousel(elementNum, carouselID, gameDataArray) {           //elementNum: int, carouselID: String, gameDataArray: array of game data obtained from the FindGames() pHp function
+    var gameNameArray = gameDataArray[0];
+    var gameLogoArray = gameDataArray[1];
+
+    var carousel = document.createElement("div");    //1
+    carousel.setAttribute("id", carouselID);
+    carousel.setAttribute("class", "carousel slide");
+    carousel.setAttribute("data-bs-slide", "carousel");
+    var carouselInner = document.createElement("div");  //2
+    carouselInner.setAttribute("class", "carousel-inner");
+    carousel.appendChild(carouselInner);
+
+    var slideNum = 0;       //3
+    var elementNumCount = 0;
+    //3 elements per slide
+    if (elementNum % 3 == 0) slideNum = elementNum / 3;              //If x number of elements is divisible by 3 then x elements can be equally split into (x/3) number of slides
+    else slideNum = Math.ceil(elementNum / 3);                       //Otherwise, if x < 3 then put x elements in a single slide and leave them be, else if there are more slides than can be split
+                                                                            //into (x/3) number of slides then leave the remaining elements in the next empty slide
+    for (let i = 1; i <= slideNum; i++) {
+        var carouselItem = document.createElement("div");
+        if (i == 1) {  
+            carouselItem.setAttribute("class", "carousel-item active"); //3a             //If first slide, then set it to active (primary slide)
+        }
+        else {
+            carouselItem.setAttribute("class", "carousel-item");   //3b
+        }
+        var row = document.createElement("div");
+        row.setAttribute("class", "row");
+        carouselInner.appendChild(carouselItem).appendChild(row);
+
+        for (let x = 1; x <= 3; x++) {                          //Repeatedly creates different elements of the same slide
+            var element = document.createElement("div");
+            element.setAttribute("class", "col-4 text-center");
+            var elementContent = document.createElement("p");           
+            elementContent.innerHTML = gameNameArray[elementNumCount];      //Temporary solution
+            /*var elementContent = document.createElement("img");           //The images are still unstable so LATER
+            elementContent.src = gameLogoArray[elementNumCount];
+            elementContent.id = "gameLogo";*/
+            row.appendChild(element).appendChild(elementContent);
+            elementNumCount++;
+        }
+    }
+
+    var carouselPrev = document.createElement("button");
+    carouselPrev.setAttribute("class", "carousel-control-prev");
+    carouselPrev.setAttribute("data-bs-target", "#" + carouselID);
+    carouselPrev.setAttribute("role", "button");
+    carouselPrev.setAttribute("data-bs-slide", "prev");
+    var prevSpan = document.createElement("span");
+    prevSpan.setAttribute("class", "carousel-control-prev-icon");
+    prevSpan.setAttribute("aria-hidden", "true");
+    carousel.appendChild(carouselPrev).appendChild(prevSpan);
+
+    var carouselNext = document.createElement("button");
+    carouselNext.setAttribute("class", "carousel-control-next");
+    carouselNext.setAttribute("data-bs-target", "#" + carouselID);
+    carouselNext.setAttribute("role", "button");
+    carouselNext.setAttribute("data-bs-slide", "next");
+    var nextSpan = document.createElement("span");
+    nextSpan.setAttribute("class", "carousel-control-next-icon");
+    nextSpan.setAttribute("aria-hidden", "true");
+    carousel.appendChild(carouselNext).appendChild(nextSpan);
+
+    return carousel;
+}
+
+function DisplayGames(divID, gameDataArray) {                              //divID: String
+    var category = document.createElement("h1");
+    if (divID.includes("new")) {
+        category.innerHTML = "Game MỚI bạn nên thử";
+        var carouselLocation = document.getElementById(divID);
+        carouselLocation.appendChild(CreateCarousel(gameDataArray[0].length, "gameCarouselNew", gameDataArray));    //Either gameDataArray[0] or gameDataArray[1] for param 1, since both
+                                                                                                                        //have the same length
+    }
 }
--- php/php_functions.php
+++ php/php_functions.php
@@ -6,7 +6,7 @@
         $DB_NAME = 'dpreqxgt_9aminghats';
         $connect = mysqli_connect($DB_SERVER, $DB_USER, $DB_PASSWORD, $DB_NAME);
         if (!$connect) {
-            die("Connection error: " . mysqli_error());
+            die("Connection error: " . mysqli_connect_error());
         }
         else {
             return $connect;   //Pass returned value to mysqli_query
@@ -28,6 +28,14 @@
         }
     }
 
+    function ArrayStartAppend($array, $newValue) {
+        for ($i = 0; $i <= count($array) - 1; $i++) {
+            $array[$i + 1] = $array[$i];
+        }
+        $array[0] = $newValue;
+        return $array;
+    }
+
     function CreatePage($name, $content, $dir) {
         $template = FileProcessing($_SERVER["DOCUMENT_ROOT"] . "pages/template/blank_template.php");
         $template_index = strpos($template, '<div class="row" id="main_content">');
@@ -42,16 +50,54 @@
         return $date;
     }
 
-    function FindGames($new, $free, $genre, $recom, $date) {
+    /*
+    Mode:
+        1: Fetch new games
+        2: Fetch free games
+        3: Fetch recommended games
+        4: Fetch favorite genres
+    */
+    function FindGames($mode, $maxDisplay) {        
         $foundGames = array();
-        if ($new) {
+        $foundLogoFile = array();
+        if ($mode == 1) {
             $today = date_create_from_format("d/m/Y", date('d/m/Y'));
-            $diff = $today->getTimestamp() - $date->getTimestamp();
-            $diff = $diff / 60 / 60 / 24 / 365;         //Calculator time interval from seconds to years
-            if ($diff <= 1) {
-                array_push($foundGames);
+            $date_raw = FetchFromDB(ConnectDB(), "SELECT RELEASEDATE FROM DB_GAMES");
+            $gamesData = FetchFromDB(ConnectDB(), "SELECT `NAME` FROM DB_GAMES")->fetch_all();
+            if ($date_raw->num_rows > 1) {
+                $date_rows = $date_raw->fetch_all();
+                for ($x = 0; $x <= count($date_rows) - 1; $x++) {
+                    $date = date_create_from_format("d/m/Y", $date_rows[$x][0]);
+                    $diff = $today->getTimestamp() - $date->getTimestamp();
+                    $diff = $diff / 60 / 60 / 24 / 365;         //Calculator time interval from seconds to years 
+                    if ($diff <= 1) {
+                        array_push($foundGames, $gamesData[$x][0]);
+                    }
+                }
+                shuffle($foundGames);
+                if (count($foundGames) > $maxDisplay) {                         //In case user does not want to display all the games found and only a portion of them
+                    array_splice($foundGames, $maxDisplay, count($foundGames) - 1, null);
+                }
+                for ($x = 0; $x <= count($foundGames) - 1; $x++) {
+                    $game = $foundGames[$x];
+                    $logoDir = $_SERVER["DOCUMENT_ROOT"] . "/resources/DB_GAMES/" . $game . "/";
+                    array_push($foundLogoFile, "resources/DB_GAMES/" . $game . "/" . GetFileNoExtension($logoDir, "logo"));
+                }
             }
         }
-        return $foundGames;
+        $foundGameData = array(
+            0 => $foundGames,
+            1 => $foundLogoFile
+        );
+        return $foundGameData;
+    }
+
+    function GetFileNoExtension($dir, $fileName) {          //Get the name of a file regardless of its extension (might cause bug if there are two files with identical names but different extension)
+        $propertyList = scandir($dir);
+        for ($x = 0; $x <= count($propertyList) - 1; $x++) {
+            if (strpos($propertyList[$x], $fileName) !== false) { 
+                return $propertyList[$x]; 
+            }
+        } 
     }
 ?>
