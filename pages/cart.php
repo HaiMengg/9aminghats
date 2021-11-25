7627e630be392beea13dd0a9f37df4f42b2cbc99
MH3323 <minhhuy3323@outlook.com.vn>
2021-11-25 22:46:57 +0700

    Add cart and aboutus

---
+++ pages/cart.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="vi">
+     <head>
+        <title>Giỏ hàng | 9amingHats</title>
+        <meta charset="utf-8">
+        <!-- <base href="https://high-man.com/"> -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" sizes="180x180" href="../resources/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="../resources/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="../resources/favicon/favicon-16x16.png">
+        <link rel="manifest" href="../resources/favicon/site.webmanifest">
+        <link rel="stylesheet" href="..\resources\plugins\bootstrap\bootstrap-5.1.3-dist\css\bootstrap.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+        <script src="..\resources\plugins\jQuery\jquery-3.6.0.js"></script>
+        <script src="..\resources\plugins\bootstrap\bootstrap-5.1.3-dist\js\bootstrap.js"></script>
+        <link rel="stylesheet" href="../css/cart_MH.css">
+    </head>
+    <body>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 justify-content-center" id="main">
+            <div class="row row_content">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 content">
+                    <div class="row">
+                        <a href="https://www.youtube.com/watch?v=b41yyHH6Cic">
+                            <img src="../resources/Payment/arrow.png">
+                        </a>
+                    </div>  
+                    <div class="row text-center">
+                        <h1>XÁC NHẬN THANH TOÁN</h1>
+                    </div>
+                    <!-- Table -->
+                    <div class="row row_data">
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <!-- header_table -->
+                            <div class="row header_table">
+                                <div class="col-md-2 col-sm-2 text-center">STT</div>
+                                <div class="col-md-5 col-sm-5 text-center">Tên game</div>
+                                <div class="col-md-3 col-sm-3">Giá</div>
+                                <div class="col-md-2 col-sm-2 text-center">Check</div>
+                            </div>
+                            <!-- data -->
+                            <div class="row" style="height: 45vh;">
+                                <div class="container data">
+                                    <div class="row" style="margin: 20px 0px 20px 0px;">
+                                        <!-- STT -->
+                                        <div class="col-md-2 col-sm-2 text-center">1</div>
+                                        <!-- Name -->
+                                        <div class="col-md-5 col-sm-5">Tên game</div>
+                                        <!-- Cost -->
+                                        <div class="col-md-3 col-sm-3">Giá</div>
+                                        <!-- Check -->
+                                        <div class="col-md-2 col-sm-2" style="padding-left: 7%;">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="" id="click_to_buy">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- footer_table -->
+                            <div class="row footer_table">
+                                <div class="col-md-2 col-sm-2 text-center">Tổng</div>
+                                <div class="col-md-5 col-sm-5"></div>
+                                <div class="col-md-3 col-sm-3">Giá</div>
+                                <div class="col-md-2 col-sm-2"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row button">
+                        <button onclick="window.location.href = 'index.php';">Xác nhận</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
